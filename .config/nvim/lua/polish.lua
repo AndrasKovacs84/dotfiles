ed1fc4671d9592f28ae426dc346a481c2e3323fb
Andras Kovacs <ka841001@gmail.com>
2025-08-21 20:42:45 +0200

    polish.lua cleanup

--- .config/nvim/lua/polish.lua
+++ .config/nvim/lua/polish.lua
@@ -2,4 +2,8 @@ if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
--- fit in the normal config locations above can go here
+-- fit in the normal config locations ab
+-- Load pywal colors if they exist
+
+-- Optionally, set the colorscheme to wal (if you're symlinking it)
+-- vim.cmd("colorscheme wal")
