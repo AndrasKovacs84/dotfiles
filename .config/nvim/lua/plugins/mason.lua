b3d72dd6a917e8d7f8673a0e6bac63b82bc5c7a3
Andras Kovacs <ka841001@gmail.com>
2025-08-24 14:53:43 +0200

    nvim debugger setup wip

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Mason
 
 ---@type LazySpec
@@ -19,6 +17,7 @@ return {
 
         -- install debuggers
         "debugpy",
+        "codelldb", --adapter to be used witgh nvim-dap for gdb
 
         -- install any other package
         "tree-sitter-cli",
