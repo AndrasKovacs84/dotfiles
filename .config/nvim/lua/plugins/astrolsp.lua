84a9d70dc25ebbe4230aa2fb95488dc9e5749216
Andras Kovacs <ka841001@gmail.com>
2025-09-03 20:37:26 +0200

    clang-tidy setup

--- .config/nvim/lua/plugins/astrolsp.lua
+++ .config/nvim/lua/plugins/astrolsp.lua
@@ -29,7 +29,7 @@ return {
             disabled = { -- disable formatting capabilities for the listed language servers
                 -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
                 -- "lua_ls",
-                "clangd"
+                "clangd", -- want to use clang-format instead for code formatting
             },
             timeout_ms = 1000, -- default format timeout
             -- filter = function(client) -- fully override the default formatting function
@@ -38,15 +38,30 @@ return {
         },
         -- enable servers that you already have installed without mason
         servers = {
+            -- "clangd",
             -- "pyright"
         },
         -- customize language server configuration options passed to `lspconfig`
         ---@diagnostic disable: missing-fields
         config = {
-            -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
+            clangd = {
+                cmd = {
+                    "/usr/bin/clangd",
+                    "--clang-tidy",
+                    "--clang-tidy-checks=*",
+                },
+                root_dir = function(fname)
+                    return require("lspconfig.util").root_pattern("compile_commands.json", ".clang-tidy", ".git")(fname)
+                        or vim.loop.cwd()
+                end,
+            },
         },
         -- customize how language servers are attached
         handlers = {
+            clangd = function(_, opts)
+                -- Override Mason's handler and manually setup your own
+                require("lspconfig").clangd.setup(opts)
+            end,
             -- a function without a key is simply the default handler, functions take two parameters, the server name and the configured options table for that server
             -- function(server, opts) require("lspconfig")[server].setup(opts) end
 
--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -2,26 +2,26 @@
 
 ---@type LazySpec
 return {
-  -- use mason-tool-installer for automatically installing Mason packages
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    -- overrides `require("mason-tool-installer").setup(...)`
-    opts = {
-      -- Make sure to use the names found in `:Mason`
-      ensure_installed = {
-        -- install language servers
-        "lua-language-server",
+    -- use mason-tool-installer for automatically installing Mason packages
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        -- overrides `require("mason-tool-installer").setup(...)`
+        opts = {
+            -- Make sure to use the names found in `:Mason`
+            ensure_installed = {
+                -- install language servers
+                "lua-language-server",
 
-        -- install formatters
-        "stylua",
+                -- install formatters
+                "stylua",
 
-        -- install debuggers
-        "debugpy",
-        "codelldb", --adapter to be used witgh nvim-dap for gdb
+                -- install debuggers
+                "debugpy",
+                "codelldb", --adapter to be used witgh nvim-dap for gdb
 
-        -- install any other package
-        "tree-sitter-cli",
-      },
+                -- install any other package
+                "tree-sitter-cli",
+            },
+        },
     },
-  },
 }
