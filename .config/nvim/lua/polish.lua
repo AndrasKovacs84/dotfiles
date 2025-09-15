e5f16587976838475cd37ca53fcf23af149cd02a
Andras Kovacs <ka841001@gmail.com>
2025-09-15 21:01:48 +0200

    italic font settings

--- .config/nvim/lua/polish.lua
+++ .config/nvim/lua/polish.lua
@@ -1,5 +1,3 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations ab
@@ -7,3 +5,15 @@ if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- Optionally, set the colorscheme to wal (if you're symlinking it)
 -- vim.cmd("colorscheme wal")
+-- vim.api.nvim_set_hl(0, "Comment", { italic = true })
+-- vim.api.nvim_set_hl(0, "String", { italic = true })
+--
+local function add_italic_to_group(group)
+    local hl = vim.api.nvim_get_hl(0, { name = group })
+    hl.italic = true
+    vim.api.nvim_set_hl(0, group, hl)
+end
+
+-- Add italics to specific groups
+add_italic_to_group "Comment"
+add_italic_to_group "String"
